cdd57ea6bf74207b1468b19d2ecc0be777758767
Nat <nat.wrw@gmail.com>
2015-02-14 22:11:41 -0800

    Fix  workaround nested callback handler bug.

--- mqtttest.lua
+++ mqtttest.lua
@@ -1,22 +1,26 @@
-local myFn = function(mac, ip)
-    print("MAC ADDRESS: " .. mac, ip)
+myFn = function(mac, ip)
+    --print("MAC ADDRESS: " .. mac .. ip)
     -- init mqtt client with keepalive timer 120sec
-    m = mqtt.Client("clientid", 120, "", "")
+    m = mqtt.Client("clientid", 120, "", "");
 
     -- setup Last Will and Testament (optional)
     -- Broker will publish a message with qos = 0, retain = 0, data = "offline" 
     -- to topic "/lwt" if client don't send keepalive packet
-    m:lwt("/lwt", "offline", 0, 0)
+    m:lwt("/lwt", "offline", 0, 0);
 
-    create_callback = function(text)
-      return function() print(text) end;
+    local create_callback = function(text)
+      local f =  function() print(text) end;
+	  return f
     end
 
-    m:on("connect", function()
-        create_callback("connected")()
-        print("CONNECTED")
-        m:publish("/topic/nat", "MAC:"..mac.." IP"..ip.. "CONNECTED",0,0, create_callback("sent"))
+    m:on("connect", function(conn)
+      m:subscribe("/topic",0, function() 
+		print("subscribed") 
+		local str = "MAC: " .. mac .. " IP: " .. ip 
+        m:publish("/topic/nat", str ,0,0, create_callback("sent"))
+	  end)
     end)
+	
     m:on("offline", create_callback("offline"))
 
     -- on publish message receive event
@@ -27,22 +31,15 @@ local myFn = function(mac, ip)
       end
     end)
 
-    -- for secure: m:connect("iot.eclipse.org", 1880, 1)
-    connectedFn = function(conn) 
-      print("connected")
-      m:subscribe("/topic",0, create_callback("subscribed"))
-    end
-
-    m:connect("iot.eclipse.org", 1883, 0,  connectedFn)
+    m:connect("iot.eclipse.org", 1883, 0);
 
     -- subscribe topic with qos = 0
     --m:subscribe("/topic",0, function(conn) print("subscribe success") end)
     -- or subscribe multiple topic (topic/0, qos = 0; topic/1, qos = 1; topic2 , qos = 2)
     -- m:subscribe({["topic/0"]=0,["topic/1"]=1,topic2=2}, function(conn) print("subscribe success") end)
     -- publish a message with data = hello, QoS = 0, retain = 0
-    m:publish("/topic","hello",0,0, create_callback("sent"))
 
-m:close();  
+	m:close();
 end
 
 return myFn
